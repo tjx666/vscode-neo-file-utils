40b71e6d822ef064a4451b654dc2776fe37ba0bd
changfeng <changfeng@gaoding.com>
2022-12-04 04:11:47 +0800

    feat: Go to Symbolic link real path

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
-# vscode extension boilerplate
+# Neo File Utils
 
-![test](https://github.com/tjx666/awesome-vscode-extension-boilerplate/actions/workflows/test.yml/badge.svg) [![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat)](http://makeapullrequest.com) [![Known Vulnerabilities](https://snyk.io/test/github/tjx666/awesome-vscode-extension-boilerplate/badge.svg?targetFile=package.json)](https://snyk.io/test/github/tjx666/awesome-vscode-extension-boilerplate?targetFile=package.json) [![Percentage of issues still open](https://isitmaintained.com/badge/open/tjx666/awesome-vscode-extension-boilerplate.svg)](http://isitmaintained.com/project/tjx666/awesome-vscode-extension-boilerplate') [![LICENSE](https://img.shields.io/badge/license-Anti%20996-blue.svg?style=flat-square)](https://github.com/996icu/996.ICU/blob/master/LICENSE)
+![test](https://github.com/tjx666/vscode-neo-file-utils/actions/workflows/test.yml/badge.svg) [![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat)](http://makeapullrequest.com) [![Known Vulnerabilities](https://snyk.io/test/github/tjx666/vscode-neo-file-utils/badge.svg?targetFile=package.json)](https://snyk.io/test/github/tjx666/vscode-neo-file-utils?targetFile=package.json) [![Percentage of issues still open](https://isitmaintained.com/badge/open/tjx666/vscode-neo-file-utils.svg)](http://isitmaintained.com/project/tjx666/vscode-neo-file-utils') [![LICENSE](https://img.shields.io/badge/license-Anti%20996-blue.svg?style=flat-square)](https://github.com/996icu/996.ICU/blob/master/LICENSE)
 
-An awesome boilerplate for VSCode extension development. You can use it just as described in the [official documentation](https://code.visualstudio.com/api).
+provide some file utils for vscode.
+
+## Features
+
+### Go to Symbolic link real path
+
+Just right click the explorer menu and select:
+
+- Open Symbolic Link Real File
+- Reveal Symbolic Link Real Folder
+
+![Go to Symbolic link real path](https://github.com/tjx666/vscode-neo-file-utils/blob/master/assets/screenshots/go-to-symbolic-link-real-path.gif?raw=true)
 
 ## My extensions
 
--- src/extension.ts
+++ src/extension.ts
@@ -1,32 +1,36 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import fs from 'node:fs/promises';
+import vscode, { Uri } from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log(
-        'Congratulations, your extension "awesome-vscode-extension-boilerplate" is now active!',
-    );
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-    const disposable = vscode.commands.registerCommand(
-        'awesome-vscode-extension-boilerplate.helloWorld',
-        () => {
-            // The code you place here will be executed every time your command is executed
-            // Display a message box to the user
-            vscode.window.showInformationMessage(
-                'Hello World from Awesome VSCode Extension Boilerplate!',
-            );
-        },
-    );
-
-    context.subscriptions.push(disposable);
+async function openSymbolicLinkRealFile(uri: Uri) {
+    const realPath = await fs.realpath(uri.fsPath);
+    vscode.commands.executeCommand('vscode.open', Uri.file(realPath));
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+async function revealSymbolicLinkRealFolder(uri: Uri) {
+    const realPath = await fs.realpath(uri.fsPath);
+    vscode.commands.executeCommand('revealInExplorer', Uri.file(realPath));
+}
+
+export function activate({ subscriptions }: vscode.ExtensionContext) {
+    vscode.commands.registerCommand(
+        'vscode-neo-file-utils.openSymbolicLinkRealFile',
+        (uri: Uri) => {
+            if (uri) {
+                openSymbolicLinkRealFile(uri);
+            }
+        },
+        subscriptions,
+    );
+
+    vscode.commands.registerCommand(
+        'vscode-neo-file-utils.revealSymbolicLinkRealFolder',
+        (uri: Uri) => {
+            if (uri) {
+                revealSymbolicLinkRealFolder(uri);
+            }
+        },
+        subscriptions,
+    );
+}
+
+// export function deactivate() {}

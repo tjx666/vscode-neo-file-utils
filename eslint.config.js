eb5174dee3176816aa0faea0ed0db82de0907699
YuTengjing <ytj2713151713@gmail.com>
2025-04-25 11:34:26 +0800

    build: upgrade deps

---
+++ eslint.config.js
@@ -0,0 +1,4 @@
+import yutengjingEslintConfigTypescript from '@yutengjing/eslint-config-typescript';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([yutengjingEslintConfigTypescript]);

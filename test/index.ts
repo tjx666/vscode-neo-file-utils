268d62f30d69082d2af8d2e6d9b88a88479bae01
YuTengjing <ytj2713151713@gmail.com>
2025-07-13 14:21:08 +0800

    build: fix tests setup

--- test/index.ts
+++ test/index.ts
@@ -1,23 +1,25 @@
+// eslint-disable-next-line @eslint-community/eslint-comments/disable-enable-pair
+/* eslint-disable promise/no-callback-in-promise */
 import path from 'node:path';
 
 import { glob } from 'glob';
 import Mocha from 'mocha';
 
-export async function run(
-    testsRoot: string,
-    callback: (error: any, failures?: number) => void,
-): Promise<void> {
+// !: can't be async function
+export function run(testsRoot: string, callback: (error: any, failures?: number) => void): void {
     const mocha = new Mocha({ color: true });
 
-    try {
-        const files = await glob('**/**.test.js', { cwd: testsRoot });
-        for (const f of files) {
-            mocha.addFile(path.resolve(testsRoot, f));
-        }
-        mocha.run((failures) => {
-            callback(null, failures);
+    glob('**/**.test.js', { cwd: testsRoot })
+        .then((files) => {
+            for (const f of files) {
+                mocha.addFile(path.resolve(testsRoot, f));
+            }
+
+            mocha.run((failures) => {
+                callback(null, failures);
+            });
+        })
+        .catch((error) => {
+            callback(error);
         });
-    } catch (error) {
-        callback(error);
-    }
 }
